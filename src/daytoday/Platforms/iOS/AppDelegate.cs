363797a67004b27b0e11ada136e87bc3063a278f
Kacper Pelo <kacperpe38@gmail.com>
2025-03-18 15:45:42 +0100

    Dodaj pliki projektów.

---
+++ src/daytoday/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace daytoday
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
