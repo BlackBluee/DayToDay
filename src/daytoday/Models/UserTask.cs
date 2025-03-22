0f5f91c36697288199df4c07c5d046391941a81a
BlackBluee <kacperpe38@gmail.com>
2025-03-22 22:59:23 +0100

    update UserTask Model

--- src/daytoday/Models/UserTask.cs
+++ src/daytoday/Models/UserTask.cs
@@ -11,6 +11,16 @@ namespace daytoday.Models
     {
         public string Title { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+
+        public string Priority { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime Due { get; set; }
+        public DateTime Updated { get; set; } = DateTime.Now;
+
+        public DateTime Completed { get; set; } = DateTime.Now;
         public string Time { get; set; }
     }
 }
